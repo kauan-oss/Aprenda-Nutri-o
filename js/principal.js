51657a33a3040937b4b9324ea810431ea2bee395
Kauan <kauan.stipp.pedroso@escola.pr.gov.br>
2025-10-07 09:00:50 -0300

    a

--- js/principal.js
+++ js/principal.js
@@ -10,7 +10,7 @@ console.log(paciente);
 var tdPeso = paciente.querySelector(".info-peso")
 var peso = tdPeso.textContent;
 console.log(tdPeso);
-var tdAltura = document.querySelector(".info-altura")
+var tdAltura = document.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 console.log(altura);
         
