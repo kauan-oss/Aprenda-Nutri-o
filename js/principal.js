420b0a02524ab04055a74ce88c71314f0f195a8c
Kauan <kauan.stipp.pedroso@escola.pr.gov.br>
2025-10-07 09:05:50 -0300

    ,

--- js/principal.js
+++ js/principal.js
@@ -7,10 +7,10 @@ titulo.textContent="Kauan Nutricions";
 
 var paciente = document.querySelector("#primeiro-paciente");
 console.log(paciente);
-var tdPeso = paciente.querySelector(".info-peso")
+var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 console.log(tdPeso);
-var tdAltura = document.querySelector(".info-altura");
+var tdAltura = document.querySelector(".info-altura");       
 var altura = tdAltura.textContent;
 console.log(altura);
         
