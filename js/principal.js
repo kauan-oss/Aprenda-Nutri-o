1560b82394e555a0f85c4677a5c3b47aabcc9828
Kauan <kauan.stipp.pedroso@escola.pr.gov.br>
2025-11-04 08:42:01 -0300

    a

--- js/principal.js
+++ js/principal.js
@@ -59,41 +59,31 @@ titulo.addEventListener("click", function(){
 
 // EU QUERO CHAMAR UM EVENTO DE CLICK QUANDO CLICAR NO BOTAO ADICIONAR
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event) {
+botaoAdicionar.addEventListener("click", function (event) { //abre chaves do botao
     event.preventDefault();
-    // alert("Oi eu sou o botao e fui clicado");
+    alert("Oi eu sou o botao e fui clicado");
     var form = document.querySelector("#form-adiciona");
 
     var nome = form.nome.value;
     var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+    var altura form.altura.value;
+    var gordura form.gordura.value;
 
+    //28/10
     var pacienteTr = document.createElement("tr");
-
     var nomeTd = document.createElement("td");
     var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
+    var alturald = document.createElement("td");
     var gorduraTd = document.createElement("td");
     var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
+    = nome; nomeTd.textContent
     pesoTd.textContent = peso;
     alturaTd.textContent = altura;
     gorduraTd.textContent = gordura;
-
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
-
-    var tabela = document.querySelector("#tabela-pacientes");
-
+    var tabela document.querySelector("#tabela-pacientes");
     tabela.appendChild(pacienteTr);
-});
-
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event)) {
-    event.preventDefault();
-    alert("Oi eu sou o botao e fui clicado")
-};
+    }); //fecha chaves do botao e fecha o parenteses da funcao
